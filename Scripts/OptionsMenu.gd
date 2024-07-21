9daf92057af856584c59663ec3a0948a09e23ea0
Kobe Della Favory <144680099+kobeDevStuff@users.noreply.github.com>
2024-07-21 22:22:51 +0800

    Added Pause menu and options menu
    Only audio options, imported from another project

--- Scripts/OptionsMenu.gd
+++ Scripts/OptionsMenu.gd
@@ -1,9 +1,11 @@
 extends Control
 
+var main_scene := "res://Scenes/world.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	ResourceLoader.load_threaded_request(main_scene)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,6 +14,9 @@ func _process(delta):
 
 
 func _on_confirm_pressed():
+	var main = ResourceLoader.load_threaded_get(main_scene)
+	get_tree().change_scene_to_packed(main)
 	AudioServer.set_bus_volume_db(0, linear_to_db($AudioOptions/VBoxContainer/MasterSlider.value))
 	AudioServer.set_bus_volume_db(1, linear_to_db($AudioOptions/VBoxContainer/SFXSlider.value))
 	AudioServer.set_bus_volume_db(2, linear_to_db($AudioOptions/VBoxContainer/MusicSlider.value))
+	
--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,20 +1,28 @@
 extends Control
-@onready var optionsMenu = preload("res://options_menu.tscn")
+var options_menu := "res://Scenes/options_menu.tscn"
+
+
+
 func _ready():
+	ResourceLoader.load_threaded_request(options_menu)
 	$AnimationPlayer.play("RESET")
 
 func resume():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
+	await $AnimationPlayer.animation_finished
+	self.visible = false
 
 func pause():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().paused = true
 	$AnimationPlayer.play("blur")
 
 func testEsc():
-	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+	if Input.is_action_just_pressed("ESC") and !get_tree().paused:
 		pause()
-	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+	elif Input.is_action_just_pressed("ESC") and get_tree().paused:
 		resume()
 
 
@@ -31,5 +39,5 @@ func _process(delta):
 
 func _on_options_pressed():
 	resume()
-	get_tree().change_scene_to_file("res://options_menu.tscn")
-
+	var options_scene = ResourceLoader.load_threaded_get(options_menu)
+	get_tree().change_scene_to_packed(options_scene)
